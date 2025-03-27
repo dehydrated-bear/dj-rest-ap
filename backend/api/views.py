f4e7d5a98aef135cab0570820145dc297e1f0f98
dehydrated bear <harshit01820@gmail.com>
2025-03-27 17:38:21 +0530

    initializingba

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,6 @@
-from django.http import JsonResponse
+from django.http import JsonResponse,HttpResponse
 import json
+from django.forms.models import model_to_dict
 from product.models import Product
 
 
@@ -10,11 +11,29 @@ def api_home(request,*args,**kwargs):
     data={}
 
     if model_data:
-        data["title"]=model_data.title
-        data["content"]=model_data.content
-        data["price"]=model_data.price
+        # data["id"]=model_data.id
+        # data["title"]=model_data.title
+        # data["content"]=model_data.content
+        # data["price"]=model_data.price
+         
+        data=model_to_dict(model_data , fields=['id','title','price'])
 
     return JsonResponse(data)
+
+        # print(data)
+        # print(type(data))
+
+        # # data=dict(data)
+
+        # data['price']=str(data['price'])
+        
+
+        # random_name=json.dumps(data)
+
+        # print(random_name[2:9],type(random_name))
+
+
+    # return HttpResponse(random_name , headers={"content-type":"application/json"})
 
 
 
--- py_client/basic.py
+++ py_client/basic.py
@@ -6,7 +6,8 @@ import requests
 # endpoint="https://httpbin.org/anything"
 endpoint="http://localhost:8000/api"
 
-get_response=requests.get(endpoint ,json={"query":"hello world"})
-print(get_response.text)
+get_response=requests.get(endpoint ,json={"product_id":123})
+# print(get_response.text)
+# print(get_response.headers)
 print(get_response.json())
-print(get_response.status_code)
+# print(get_response.status_code)
